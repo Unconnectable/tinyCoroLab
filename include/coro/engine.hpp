03f75eae0acc96a2701239a90880920747726cea
pink-server <scpr7@outlook.com>
2026-03-23 21:45:06 +0800

    doc: 未完成的lab2, 还有段错误需要修改

--- include/coro/engine.hpp
+++ include/coro/engine.hpp
@@ -28,7 +28,7 @@ class context;
 };
 
 /**
- * @brief Welcome to tinycoro lab2a, in this part you will build the heart of tinycoro¡ª¡ªengine by
+ * @brief Welcome to tinycoro lab2a, in this part you will build the heart of tinycoroï¿½ï¿½ï¿½ï¿½engine by
  * modifing engine.hpp and engine.cpp, please ensure you have read the document of lab2a.
  *
  * @warning You should carefully consider whether each implementation should be thread-safe.
@@ -170,7 +170,9 @@ public:
 private:
     uint32_t    m_id;
     uring_proxy m_upxy;
-
+    atomic<uint32_t> m_running_io = 0;
+    // uint32_t    m_io_to_submit;
+    atomic<uint32_t> m_io_to_submit = 0;
     // store task handle
     mpmc_queue<coroutine_handle<>> m_task_queue; // You can replace it with another data structure
 
--- src/engine.cpp
+++ src/engine.cpp
@@ -9,40 +9,55 @@ using std::memory_order_relaxed;
 auto engine::init() noexcept -> void
 {
     // TODO[lab2a]: Add you codes
+    m_upxy.init(config::kQueCap);
+    linfo.egn      = this;
+    m_running_io   = 0;
+    m_io_to_submit = 0;
 }
 
 auto engine::deinit() noexcept -> void
 {
     // TODO[lab2a]: Add you codes
+    m_upxy.deinit();
+    m_running_io   = 0;
+    m_io_to_submit = 0;
+    // coroutine_handle<> handle;
+    // while (m_task_queue.try_pop(handle))
+    // {
+    //     handle.destroy();
+    // }
 }
 
 auto engine::ready() noexcept -> bool
 {
     // TODO[lab2a]: Add you codes
-    return {};
+    return m_task_queue.was_size() > 0;
 }
 
 auto engine::get_free_urs() noexcept -> ursptr
 {
     // TODO[lab2a]: Add you codes
-    return {};
+    return m_upxy.get_free_sqe();
 }
 
 auto engine::num_task_schedule() noexcept -> size_t
 {
     // TODO[lab2a]: Add you codes
-    return {};
+    return m_task_queue.was_size();
 }
 
 auto engine::schedule() noexcept -> coroutine_handle<>
 {
     // TODO[lab2a]: Add you codes
-    return {};
+    return m_task_queue.pop();
 }
 
 auto engine::submit_task(coroutine_handle<> handle) noexcept -> void
 {
     // TODO[lab2a]: Add you codes
+    m_task_queue.push(handle);
+    m_upxy.write_eventfd(1);
+    // wake_up();
 }
 
 auto engine::exec_one_task() noexcept -> void
@@ -64,16 +79,58 @@ auto engine::handle_cqe_entry(urcptr cqe) noexcept -> void
 auto engine::poll_submit() noexcept -> void
 {
     // TODO[lab2a]: Add you codes
+    auto submit = [&]()
+    {
+        if (m_io_to_submit > 0)
+        {
+            int submitted_count = m_upxy.submit();
+            m_running_io.fetch_add(m_io_to_submit);
+            // m_io_to_submit.fetch_sub(submitted_count);
+            m_io_to_submit = 0;
+        }
+    };
+    submit();
+    // 只有在没有计算任务,但有IO在运行时,才等待
+    auto cnt = m_upxy.wait_eventfd(); // 等待 IO 执行
+
+    if (m_task_queue.was_size() == 0 && m_running_io > 0)
+    {
+        m_upxy.wait_eventfd();
+    }
+
+    // 获取已经完成的状态,然后减去
+
+    // auto num =            m_upxy.peek_batch_cqe(m_urc.data(), m_num_io_running.load(std::memory_order_acquire));
+
+    int completed_count = m_upxy.peek_batch_cqe(m_urc.data(), m_running_io);
+
+    if (completed_count > 0)
+    {
+        m_running_io.fetch_sub(completed_count);
+
+        for (int i = 0; i < completed_count; ++i)
+        {
+            handle_cqe_entry(m_urc[i]);
+        }
+        m_upxy.cq_advance(completed_count);
+        m_running_io = m_running_io - completed_count;
+    }
 }
 
 auto engine::add_io_submit() noexcept -> void
 {
     // TODO[lab2a]: Add you codes
+    m_io_to_submit.fetch_add(1);
 }
 
 auto engine::empty_io() noexcept -> bool
 {
     // TODO[lab2a]: Add you codes
-    return {};
+    if (m_running_io == 0 and m_io_to_submit == 0)
+    {
+        return true;
+    }
+
+    return false;
 }
 }; // namespace coro::detail
